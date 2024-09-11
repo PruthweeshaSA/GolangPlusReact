68ece514ec8caf3a21e3d3ffcbc4f90ae86859c8
PruthweeshaSA <pruthweeshasa@gmail.com>
2024-09-11 16:56:07 +0530

    updated codebase

--- tokens.py
+++
@@ -1,4 +0,0 @@
-username = "pruthweeshasa"
-password = "5Y6901VHacmSdzyq"
-mongodb_uri = "mongodb+srv://pruthweeshasa:5Y6901VHacmSdzyq@clusterlxix.fev79.mongodb.net/?retryWrites=true&w=majority&appName=ClusterLXIX"
-
